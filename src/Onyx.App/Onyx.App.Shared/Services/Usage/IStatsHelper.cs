31266b869ac4609198a02f9fd5db38a4d501ace3
Leonhard-Neuhold <leonhard.neuhold@outlook.de>
2025-03-07 12:56:44 +0100

    improvements to StatsHelper

--- src/Onyx.App/Onyx.App.Shared/Services/Usage/IStatsHelper.cs
+++ src/Onyx.App/Onyx.App.Shared/Services/Usage/IStatsHelper.cs
@@ -2,9 +2,5 @@
 
 public interface IStatsHelper
 {
-    List<Stats>? GetUsageStatsLastDay();
-    List<Stats>? GetUsageStatsLastWeek();
-    List<Stats>? GetUsageStatsLastMonth();
-    List<Stats>? GetUsageStatsLastYear();
     List<Stats>? GetUsageStatsTimeInterval(long startTime, long endTime);
 }
--- src/Onyx.App/Onyx.App/Platforms/Android/UsageData/StatsHelper.cs
+++ src/Onyx.App/Onyx.App/Platforms/Android/UsageData/StatsHelper.cs
@@ -8,122 +8,6 @@ namespace Onyx.App.UsageData;
 
 public class StatsHelper : IStatsHelper
 {
-    private readonly string[] _notInNames = ["android", "google", "com", "apple", "samsung"];
-
-
-    public List<Stats>? GetUsageStatsLastYear()
-    {
-        var usageStatsManager = (UsageStatsManager)Application.Context.GetSystemService(Context.UsageStatsService)!;
-        long endTime = Java.Lang.JavaSystem.CurrentTimeMillis();
-        long startTime = endTime - (365L * 24 * 60 * 60 * 1000);
-        
-        var usageStatsList = usageStatsManager.QueryUsageStats(UsageStatsInterval.Yearly, startTime, endTime);
-        if (usageStatsList == null || !usageStatsList.Any())
-        {
-            return new List<Stats>();
-        }
-        
-        return usageStatsList
-            .Select(u => new Stats()
-            {
-                Name = u.PackageName == null ? string.Empty :
-                    string.Join(" ", u.PackageName.Split('.')
-                        .Where(s => !_notInNames.Contains(s))
-                        .ToArray()).ToString(),
-                TimeInForeground = TimeSpan.FromMilliseconds(u.TotalTimeInForeground),
-#pragma warning disable CA1416
-                TimeVisible = TimeSpan.FromMilliseconds(u.TotalTimeVisible)
-#pragma warning restore CA1416
-            })
-            .Where(u => u.TimeInForeground != TimeSpan.Zero && u.TimeVisible != TimeSpan.Zero)
-            .OrderByDescending(u => u.TimeInForeground)
-            .ToList();
-    }
-    
-    public List<Stats>? GetUsageStatsLastMonth()
-    {
-        var usageStatsManager = (UsageStatsManager)Application.Context.GetSystemService(Context.UsageStatsService)!;
-        long endTime = Java.Lang.JavaSystem.CurrentTimeMillis();
-        long startTime = endTime - (30L * 24 * 60 * 60 * 1000);
-        
-        var usageStatsList = usageStatsManager.QueryUsageStats(UsageStatsInterval.Monthly, startTime, endTime);
-        if (usageStatsList == null || !usageStatsList.Any())
-        {
-            return new List<Stats>();
-        }
-        
-        return usageStatsList
-            .Select(u => new Stats()
-            {
-                Name = u.PackageName?
-                    .Split('.')
-                    .Where(s => !_notInNames.Contains(s))
-                    .ToString() ?? string.Empty,
-                TimeInForeground = TimeSpan.FromMilliseconds(u.TotalTimeInForeground),
-#pragma warning disable CA1416
-                TimeVisible = TimeSpan.FromMilliseconds(u.TotalTimeVisible)
-#pragma warning restore CA1416
-            })
-            .Where(u => u.TimeInForeground != TimeSpan.Zero && u.TimeVisible != TimeSpan.Zero)
-            .OrderByDescending(u => u.TimeInForeground)
-            .ToList();
-    }
-    
-    public List<Stats>? GetUsageStatsLastWeek()
-    {
-        var usageStatsManager = (UsageStatsManager)Application.Context.GetSystemService(Context.UsageStatsService)!;
-        long endTime = Java.Lang.JavaSystem.CurrentTimeMillis();
-        long startTime = endTime - (7 * 24 * 60 * 60 * 1000);
-        
-        var usageStatsList = usageStatsManager.QueryUsageStats(UsageStatsInterval.Weekly, startTime, endTime);
-        if (usageStatsList == null || !usageStatsList.Any())
-        {
-            return new List<Stats>();
-        }
-        
-        return usageStatsList
-            .Select(u => new Stats()
-            {
-                Name = GetAppNameFromPackage(u.PackageName ?? string.Empty),
-                TimeInForeground = TimeSpan.FromMilliseconds(u.TotalTimeInForeground),
-#pragma warning disable CA1416
-                TimeVisible = TimeSpan.FromMilliseconds(u.TotalTimeVisible)
-#pragma warning restore CA1416
-            })
-            .Where(u => u.TimeInForeground != TimeSpan.Zero && u.TimeVisible != TimeSpan.Zero)
-            .OrderByDescending(u => u.TimeInForeground)
-            .ToList();
-    }
-    
-    public List<Stats>? GetUsageStatsLastDay()
-    {
-        var usageStatsManager = (UsageStatsManager)Application.Context.GetSystemService(Context.UsageStatsService)!;
-        long endTime = Java.Lang.JavaSystem.CurrentTimeMillis();
-        long startTime = endTime - (24 * 60 * 60 * 1000);
-        
-        var usageStatsList = usageStatsManager.QueryUsageStats(UsageStatsInterval.Daily, startTime, endTime);
-        if (usageStatsList == null || !usageStatsList.Any())
-        {
-            return new List<Stats>();
-        }
-        
-        return usageStatsList
-            .Select(u => new Stats()
-            {
-                Name = u.PackageName?
-                    .Split('.')
-                    .Where(s => !_notInNames.Contains(s))
-                    .ToString() ?? string.Empty,
-                TimeInForeground = TimeSpan.FromMilliseconds(u.TotalTimeInForeground),
-#pragma warning disable CA1416
-                TimeVisible = TimeSpan.FromMilliseconds(u.TotalTimeVisible)
-#pragma warning restore CA1416
-            })
-            .Where(u => u.TimeInForeground != TimeSpan.Zero && u.TimeVisible != TimeSpan.Zero)
-            .OrderByDescending(u => u.TimeInForeground)
-            .ToList();
-    }
-
     public List<Stats>? GetUsageStatsTimeInterval(long startTime, long endTime)
     {
         var usageStatsManager = (UsageStatsManager)Application.Context.GetSystemService(Context.UsageStatsService)!;
@@ -139,9 +23,7 @@ public class StatsHelper : IStatsHelper
             {
                 Name = GetAppNameFromPackage(u.PackageName),
                 TimeInForeground = TimeSpan.FromMilliseconds(u.TotalTimeInForeground),
-#pragma warning disable CA1416
                 TimeVisible = TimeSpan.FromMilliseconds(u.TotalTimeVisible)
-#pragma warning restore CA1416
             })
             .Where(u => u.TimeInForeground != TimeSpan.Zero && u.TimeVisible != TimeSpan.Zero)
             .OrderByDescending(u => u.TimeInForeground)
@@ -165,5 +47,4 @@ public class StatsHelper : IStatsHelper
             return packageName; // Falls die App nicht gefunden wurde
         }
     }
-
 }
