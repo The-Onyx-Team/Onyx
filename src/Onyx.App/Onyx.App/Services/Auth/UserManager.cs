7486da3bec4dd6298b2576bd8396c5099875072a
Roman-Fuehrer <r.fuehrer@htlkrems.at>
2025-04-08 15:04:44 +0200

    Work gone

--- src/Onyx.App/Onyx.App/Services/Auth/UserManager.cs
+++ src/Onyx.App/Onyx.App/Services/Auth/UserManager.cs
@@ -1,17 +1,13 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Onyx.App.Services.Api;
 using Onyx.App.Shared.Services.Auth;
-using Onyx.Data.DataBaseSchema.Identity;
 
 namespace Onyx.App.Services.Auth;
 
-public class UserManager(AuthenticationStateProvider authenticationStateProvider, AuthApi api,UserManager<ApplicationUser> userManager) : IUserManager
+public class UserManager(AuthenticationStateProvider authenticationStateProvider, AuthApi api) : IUserManager
 {
-    private readonly UserManager<ApplicationUser> _userManager = userManager;
-    
     public async Task<RegisterResult> RegisterAsync(string name, string email, string password, string redirectUri)
     {
         var result = await api.RegisterAsync(name, email, password);
@@ -120,115 +116,38 @@ public class UserManager(AuthenticationStateProvider authenticationStateProvider
         return Task.FromResult<IEnumerable<ExternalLoginData>>([])!;
     }
 
-    public async Task<bool> RemoveLogin(User user, string loginProvider, string providerKey)
+    public Task<bool> RemoveLogin(User user, string loginProvider, string providerKey)
     {
-        var dbUser = await _userManager.FindByIdAsync(user.Id!);
-        var result = await _userManager.RemoveLoginAsync(dbUser!,loginProvider,providerKey);
-        return result.Succeeded;
+        throw new NotImplementedException();
     }
 
-    public async Task<bool> ChangeEmail(User user, string newEmail)
+    public Task<bool> ChangeEmail(User user, string newEmail)
     {
-        var existingUser = await _userManager.FindByEmailAsync(newEmail);
-        if (existingUser != null)
-        {
-            return false;
-        }
-        var dbUser = await _userManager.FindByIdAsync(user.Id!);
-        newEmail = newEmail.ToUpper();
-        dbUser!.Email = newEmail;
-        try
-        {
-            await _userManager.UpdateNormalizedEmailAsync(dbUser);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-        }
-        
-        return true;   
+        throw new NotImplementedException();
     }
 
-    public async Task<bool> ChangePhoneNumber(User user, string phoneNumber)
+    public Task<bool> ChangePhoneNumber(User user, string phoneNumber)
     {
-         
-        var dbUser = await _userManager.FindByIdAsync(user.Id!);
-            dbUser!.PhoneNumber = phoneNumber;
-            var result = await _userManager.UpdateAsync(dbUser);
-            
-        return result.Succeeded;
+        throw new NotImplementedException();
     }
 
-    public async Task<bool> ChangePassword(User user, string oldPassword, string newPassword)
+    public Task<bool> ChangePassword(User user, string oldPassword, string newPassword)
     {
-        var dbUser = await _userManager.FindByIdAsync(user.Id!);
-        try
-        {
-            if (oldPassword != newPassword)
-            {
-                if (dbUser!.PasswordHash == oldPassword.GetHashCode().ToString())
-                {
-                    dbUser.PasswordHash = newPassword.GetHashCode().ToString();
-                }
-                else
-                {
-                    Console.WriteLine("Old password does not match!");
-                    return false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("New Password can't be old Password!");
-                return false;
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-        }
-        var result = await _userManager.UpdateAsync(dbUser);
-        return result.Succeeded;
+        throw new NotImplementedException();
     }
 
     public Task<bool> SendChangePasswordEmail(string email)
     {
-        
         throw new NotImplementedException();
     }
 
-    public async Task<bool> ChangeUserName(User user, string userName)
+    public Task<bool> ChangeUserName(User user, string userName)
     {
-        var existingUser = await _userManager.FindByNameAsync(userName);
-        if (existingUser != null)
-        {
-            return false;
-        }
-        var dbUser = await _userManager.FindByIdAsync(user.Id!);
-        dbUser!.UserName = userName;
-        var result = await _userManager.UpdateAsync(dbUser);
-        return result.Succeeded;
+        throw new NotImplementedException();
     }
-    
 
-    public async Task<string[]> GetRecoveryCodes(User user)
+    public Task<string[]> GetRecoveryCodes(User user)
     {
-        var dbUser = await _userManager.FindByIdAsync(user.Id!);
-        try
-        {
-            if (dbUser!.TwoFactorEnabled)
-            {
-                return _userManager.GenerateNewTwoFactorRecoveryCodesAsync(dbUser, 10).Result!.ToArray();
-            }
-
-            Console.WriteLine("2Fa not enabled");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return null!;
-        }
-        return null!;
+        throw new NotImplementedException();
     }
 }
