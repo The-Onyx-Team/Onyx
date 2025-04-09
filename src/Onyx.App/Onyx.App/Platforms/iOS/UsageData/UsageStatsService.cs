791e470a899dc9f5e747c7d03ed29a2a90bbb759
gabriel <g.martin@htlkrems.at>
2025-04-09 07:27:41 +0200

    Add noimpl data services for ios and mac

---
+++ src/Onyx.App/Onyx.App/Platforms/iOS/UsageData/UsageStatsService.cs
@@ -0,0 +1,11 @@
+﻿using Onyx.App.Shared.Services.Usage;
+
+namespace Onyx.App.UsageData;
+
+public class UsageStatsService : IStatsService
+{
+    public void OpenUsageAccessSettings()
+    {
+        throw new NotImplementedException();
+    }
+}
