405618ad725f6149ec3962962724eb0c6cf6664e
Roman-Fuehrer <r.fuehrer@htlkrems.at>
2025-04-24 09:55:05 +0200

    Update DataEndpoints.cs

--- src/Onyx.App/Onyx.App.Web/Api/DataEndpoints.cs
+++ src/Onyx.App/Onyx.App.Web/Api/DataEndpoints.cs
@@ -7,39 +7,48 @@ namespace Onyx.App.Web.Api;
 public class DataEndpoints
 {
     
-    public async Task<bool> NewUsage(ApplicationDbContext context,string date, string fromTime, string toTime, string device, string app )
+    public async Task<bool> NewUsage(ApplicationDbContext context,string userid,string date, string fromTime, string toTime, string device, string app )
     {
-        DbSet<Usage> usageTable = context.Set<Usage>();
-        DbSet<Device> deviceTable = context.Set<Device>();
-        DbSet<RegisteredApp> appTable = context.Set<RegisteredApp>();
-        var newitem = new Usage();
-        int devIdhelper;
-        int appIdhelper;
-        if (usageTable.Where( a => a.Devices.Name == device).Any())
+        try
         {
-            devIdhelper = usageTable.Where( a => a.Devices.Name == device).First().Devices.Id;
-        }
-        else
-        {
-            await deviceTable.AddAsync(new Device() {Name = device,UserId = temp});
+            DbSet<Usage> usageTable = context.Set<Usage>();
+            var newitem = new Usage();
+            int devIdhelper;
+            int appIdhelper;
+            if (usageTable.Any( a => a.Devices.Name == device))
+            {
+                devIdhelper = usageTable.First( a => a.Devices.Name == device).Devices.Id;
+            }
+            else
+            {
+                DbSet<Device> deviceTable = context.Set<Device>();
+                await deviceTable.AddAsync(new Device() {Name = device,UserId = userid});
+                await context.SaveChangesAsync();
+                devIdhelper = usageTable.First( a => a.Devices.Name == device).Devices!.Id;
+            }
+            if (usageTable.Any(a => a.App.Name == device))
+            {
+                appIdhelper = usageTable.First( a => a.App.Name == device).App.Id;
+            }
+            else
+            {
+                DbSet<RegisteredApp> appTable = context.Set<RegisteredApp>();
+                await appTable.AddAsync(new RegisteredApp() {Name = app});
+                await context.SaveChangesAsync();
+                appIdhelper = usageTable.First( a => a.App.Name == device).App.Id;
+            }
+            newitem.Date = DateOnly.Parse(date);
+            newitem.Duration = new TimeSpan( (int.Parse(fromTime) - int.Parse(toTime)));
+            newitem.DeviceId = devIdhelper;
+            newitem.AppId = appIdhelper;
             await context.SaveChangesAsync();
-            devIdhelper = usageTable.Where( a => a.Devices.Name == device).First().Devices.Id;
         }
-        if (usageTable.Where( a => a.App.Name == device).Any())
+        catch (Exception e)
         {
-            appIdhelper = usageTable.Where( a => a.App.Name == device).First().App.Id;
+            Console.WriteLine(e);
+            return false;
         }
-        else
-        {
-            await appTable.AddAsync(new RegisteredApp() {Name = app});
-            await context.SaveChangesAsync();
-            appIdhelper = usageTable.Where( a => a.App.Name == device).First().App.Id;
-        }
-
-        newitem.Date = DateOnly.Parse(date);
-        newitem.Duration = new TimeSpan( (int.Parse(fromTime) - int.Parse(toTime)));
-        newitem.DeviceId = devIdhelper;
-        newitem.AppId = appIdhelper;
+       
         return true;
     }
 }
